fb2df3b043ba988aeec8b3f2b1106b63c7997aea
ho.duc.haib <SUN-ASTERISK\ho.duc.haib@b123154-pc.sun-asterisk.com>
2022-07-28 16:39:13 +0700

    update gradle, library

---
+++ presentation/src/main/java/com/example/github/extension/Function.kt
@@ -0,0 +1,10 @@
+package com.example.github.extension
+
+suspend fun <T> tryWith(call: suspend () -> T): T? {
+    return try {
+        call.invoke()
+    } catch (e: Exception) {
+        e.printStackTrace()
+        null
+    }
+}
